6b5948d6959b56526bca70afb2953b0c608f516a
Patryk <32525496+PRZEMPM@users.noreply.github.com>
2024-05-16 14:18:29 +0200

    Update Shape.hpp

--- shapes/Shape.hpp
+++ shapes/Shape.hpp
@@ -8,4 +8,14 @@ public:
     virtual double getArea() const = 0;
     virtual double getPerimeter() const = 0;
     virtual void print() const;
+
+private: 
+    enum class Color : unsigned char
+    {
+        RED = 0,
+        GREEN = 1,
+        BLUE = 2
+    };
+    
+    Color color_ = Color::RED;
 };
