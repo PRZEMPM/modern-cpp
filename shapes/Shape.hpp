50b90edcc15552abfc585b066aa41e274baf1eb7
Patryk <32525496+PRZEMPM@users.noreply.github.com>
2024-05-22 09:45:35 +0200

    Update Shape.hpp - operator<<

--- shapes/Shape.hpp
+++ shapes/Shape.hpp
@@ -1,21 +1,34 @@
 #pragma once
 
+#include <iostream>
+
+enum class Color : unsigned char
+{
+    RED = 0,
+    GREEN = 1,
+    BLUE = 2
+};
+
+std::ostream& operator<< (std::ostream & os, Color color);
+
 class Shape
 {
+protected: 
+    Color color_ = Color::RED;
+
 public:
+    Shape(Color c) : color_(c) {}
+    Shape() {}
+
+    Shape(const Shape& ref)
+    {
+        color_ = ref.color_;
+    }
+
     virtual ~Shape() {}
 
     virtual double getArea() const = 0;
     virtual double getPerimeter() const = 0;
     virtual void print() const;
-
-private: 
-    enum class Color : unsigned char
-    {
-        RED = 0,
-        GREEN = 1,
-        BLUE = 2
-    };
-    
-    Color color_ = Color::RED;
 };
+
