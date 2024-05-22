682832710e964879edc2d6aed6cc84d3e4ce12a4
Patryk <32525496+PRZEMPM@users.noreply.github.com>
2024-05-22 09:45:15 +0200

    Update Shape.cpp - operator<<

--- shapes/Shape.cpp
+++ shapes/Shape.cpp
@@ -5,3 +5,23 @@ void Shape::print() const
 {
     std::cout << "Unknown Shape" << std::endl;
 }
+
+std::ostream& operator<< (std::ostream & os, Color color)
+{
+    switch (color)
+    {
+        case Color::RED:
+            os << "RED";
+            break;
+        case Color::GREEN:
+            os << "GREEN";
+            break;
+        case Color::BLUE:
+            os << "BLUE";
+            break;
+        default:
+            os << "UNKNOWN";
+    }
+
+    return os;
+}
